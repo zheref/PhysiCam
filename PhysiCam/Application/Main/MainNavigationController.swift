3cf91c01929e0d9d23fb2580e21e3abee13bf9c7
Sergio Daniel <me@zheref.io>
2022-09-10 00:40:35 -0500

    Remove storyboard usage instead of programmatic UI

---
+++ PhysiCam/Application/Main/MainNavigationController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class MainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
+    }
+
+}
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+    
+}
---
+++ PhysiCam/Application/Take/TakeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TakeViewController.swift
+//  PhysiCam
+//
+//  Created by Sergio Daniel on 9/10/22.
+//
+
+import UIKit
+
+class TakeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
